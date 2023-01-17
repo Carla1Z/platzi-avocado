89ba05b2b388bc7b1e5ea28dccce7cfda903ec47
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-01-17 00:14:50 -0300

    me traigo la info de db

--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,7 +1,16 @@
 import { IncomingMessage, ServerResponse } from 'http'
+import DB from '@database'
 
 const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
-  response.end(JSON.stringify({ hello: 'world' }))
+  const db = new DB()
+  const allEntries = await db.getAll()
+  // getById: va a buscar un elemento en particular y lo va a retornar
+    const length = allEntries.length
+
+
+  response.statusCode = 200
+  response.setHeader('Content-type', 'application/json')
+  response.end(JSON.stringify({ data: allEntries, length }))
 }
 
 export default allAvos
